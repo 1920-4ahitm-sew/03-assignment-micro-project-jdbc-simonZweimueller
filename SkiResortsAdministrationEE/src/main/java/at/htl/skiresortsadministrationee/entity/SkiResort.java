cf8b9f6707e41013e594819b21f2c29aebe710e1
Simon Zweimueller <simon.zweimueller1@gmail.com>
2019-10-23 17:56:51 +0200

    added getters and setters

--- SkiResortsAdministrationEE/src/main/java/at/htl/skiresortsadministrationee/entity/SkiResort.java
+++ SkiResortsAdministrationEE/src/main/java/at/htl/skiresortsadministrationee/entity/SkiResort.java
@@ -1,4 +1,58 @@
 package at.htl.skiresortsadministrationee.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class SkiResort {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    String skiResortName;
+    String getSkiResortTown;
+    int lowestPoint;
+    int highestPoint;
+
+    public Long getId() {
+        return id;
+    }
+
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+
+    public String getSkiResortName() {
+        return skiResortName;
+    }
+
+    public void setSkiResortName(String skiResortName) {
+        this.skiResortName = skiResortName;
+    }
+
+    public String getGetSkiResortTown() {
+        return getSkiResortTown;
+    }
+
+    public void setGetSkiResortTown(String getSkiResortTown) {
+        this.getSkiResortTown = getSkiResortTown;
+    }
+
+    public int getLowestPoint() {
+        return lowestPoint;
+    }
+
+    public void setLowestPoint(int lowestPoint) {
+        this.lowestPoint = lowestPoint;
+    }
+
+    public int getHighestPoint() {
+        return highestPoint;
+    }
+
+    public void setHighestPoint(int highestPoint) {
+        this.highestPoint = highestPoint;
+    }
 }
