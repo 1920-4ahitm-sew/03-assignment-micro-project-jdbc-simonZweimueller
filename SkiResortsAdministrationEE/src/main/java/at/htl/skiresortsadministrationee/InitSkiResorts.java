a5038c8b2487d5126825cb21e9f5f57bbf24466d
Simon Zweimueller <simon.zweimueller1@gmail.com>
2019-10-23 18:12:21 +0200

    created SkiResorts Hoess and Wurzeralm

--- SkiResortsAdministrationEE/src/main/java/at/htl/skiresortsadministrationee/InitSkiResorts.java
+++ SkiResortsAdministrationEE/src/main/java/at/htl/skiresortsadministrationee/InitSkiResorts.java
@@ -1,4 +1,30 @@
 package at.htl.skiresortsadministrationee;
 
+import at.htl.skiresortsadministrationee.entity.SkiResort;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Destroyed;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+@Transactional
 public class InitSkiResorts {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public void init(@Observes @Initialized(ApplicationScoped.class) Object init) {
+        System.out.println("*** It works! ***");
+
+        em.persist(new SkiResort("Hoess", "Hinterstoder", 600, 2000));
+        em.persist(new SkiResort("Wurzeralm", "Spital am Pyhrn", 800, 1870));
+    }
+
+    public void tearDown(@Observes @Destroyed(ApplicationScoped.class) Object init) {
+        // when app is undeployed
+    }
 }
