a58d088d8af87f4a0610ceaa4b766fc63a35e3f6
Simon Zweimueller <simon.zweimueller1@gmail.com>
2019-10-24 19:32:34 +0200

    added named query

--- SkiResortsAdministrationEE/src/main/java/at/htl/skiresortsadministrationee/SkiResortAdministrationServlet.java
+++ SkiResortsAdministrationEE/src/main/java/at/htl/skiresortsadministrationee/SkiResortAdministrationServlet.java
@@ -1,4 +1,29 @@
 package at.htl.skiresortsadministrationee;
 
-public class SkiResortAdministrationServlet {
+import at.htl.skiresortsadministrationee.entity.SkiResort;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("servlet")
+public class SkiResortAdministrationServlet extends HttpServlet {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        //String name = req.getParameter("name");
+        //resp.getWriter().printf("Hello %s!", name);
+        TypedQuery<SkiResort> query = em.createNamedQuery("GetAllSkiResorts", SkiResort.class);
+        List<SkiResort> skiResorts = query.getResultList();
+        resp.getWriter().printf("" + skiResorts);
+    }
 }
