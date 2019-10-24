ce199a9c4451095af81aa778372580faa2e168e3
Simon Zweimueller <simon.zweimueller1@gmail.com>
2019-10-24 20:08:04 +0200

    created skiresort for se

---
+++ SkiResortsAdministrationSE/src/main/java/at/htl/skiresortsadministrationse/entity/SkiResort.java
@@ -0,0 +1,78 @@
+package at.htl.skiresortsadministrationse.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@NamedQueries(@NamedQuery(name = "GetSkiResortById", query = "select sr from SkiResort sr where sr.id = ?1"))
+public class SkiResort implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    String skiResortName;
+    String skiResortTown;
+    int lowestPoint;
+    int highestPoint;
+
+    public SkiResort(String skiResortName, String skiResortTown, int lowestPoint, int highestPoint) {
+        this.skiResortName = skiResortName;
+        this.skiResortTown = skiResortTown;
+        this.lowestPoint = lowestPoint;
+        this.highestPoint = highestPoint;
+    }
+
+    public SkiResort() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+
+    public String getSkiResortName() {
+        return skiResortName;
+    }
+
+    public void setSkiResortName(String skiResortName) {
+        this.skiResortName = skiResortName;
+    }
+
+    public String getSkiResortTown() {
+        return skiResortTown;
+    }
+
+    public void setSkiResortTown(String skiResortTown) {
+        this.skiResortTown = skiResortTown;
+    }
+
+    public int getLowestPoint() {
+        return lowestPoint;
+    }
+
+    public void setLowestPoint(int lowestPoint) {
+        this.lowestPoint = lowestPoint;
+    }
+
+    public int getHighestPoint() {
+        return highestPoint;
+    }
+
+    public void setHighestPoint(int highestPoint) {
+        this.highestPoint = highestPoint;
+    }
+
+    @Override
+    public String toString() {
+        return "SkiResort{" +
+                "id=" + id +
+                ", skiResortName='" + skiResortName + '\'' +
+                ", skiResortTown='" + skiResortTown + '\'' +
+                ", lowestPoint=" + lowestPoint +
+                ", highestPoint=" + highestPoint +
+                '}';
+    }
+}
