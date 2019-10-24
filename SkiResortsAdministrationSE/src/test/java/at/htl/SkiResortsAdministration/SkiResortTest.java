8bb464cea599ac6c819bf17c719a5133ab0818dc
Simon Zweimueller <simon.zweimueller1@gmail.com>
2019-10-24 19:48:39 +0200

    added init

--- SkiResortsAdministrationSE/src/test/java/at/htl/SkiResortsAdministration/SkiResortTest.java
+++ SkiResortsAdministrationSE/src/test/java/at/htl/SkiResortsAdministration/SkiResortTest.java
@@ -1,6 +1,20 @@
 package at.htl.SkiResortsAdministration;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
 public class SkiResortTest {
 
-
+    static EntityManager em;
+    @BeforeAll
+    private static void init() {
+        em = Persistence
+                .createEntityManagerFactory("myPU")
+                .createEntityManager();
+    }
 }
